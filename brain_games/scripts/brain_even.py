d52382e447be64e7c4f6b5090493f19ca88ccdad
Valentina <va13ntina@yandex.ru>
2023-02-08 02:00:58 +0300

    fix brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import sys
 import os
-sys.path.insert(0, os.path.join(os.getcwd(), '..'))
-import brain_games.logic
+sys.path.insert(0, os.path.join(os.getcwd(), '../..'))
+from brain_games import logic
 from brain_games.games import even
 
 
