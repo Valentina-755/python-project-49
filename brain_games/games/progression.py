778db63c2d8b8ad3f01a0596e7496152fa0c7622
Valentina <va13ntina@yandex.ru>
2023-02-12 03:46:07 +0300

    add brain_progression.python

---
+++ brain_games/games/progression.py
@@ -0,0 +1,30 @@
+from random import randint
+
+
+def condition():
+    condition = 'Find the greatest common divisor of given numbers.'
+    print(condition)
+
+
+def task():
+    arguments = {}
+    counter = 0
+    while counter < 3:
+        start = randint(1, 50)
+        step = randint(1, 10)
+        skip = randint(0, 9)
+        task = str(start)
+        quantity = 1
+        while quantity < 10:
+            if quantity != skip:
+                task += ' ' + str(start + step)
+                start += step
+                quantity += 1
+            else:
+                task += ' ..'
+                correct_answer = start + step
+                start += step
+                quantity += 1
+        arguments[task] = str(correct_answer)
+        counter += 1
+    return arguments
